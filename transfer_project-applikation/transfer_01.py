887bedd611fc2bf6cb4d0b325e0a8eb75c720a6b
btemperli <beat@temper.li>
2025-09-04 10:25:49 +0200

    add first transfer-solution

---
+++ transfer_project-applikation/transfer_01.py
@@ -0,0 +1,21 @@
+# ----------------------------
+# Transfer-Aufgabe 01.
+# Interaktiver Kreis.
+# Mögliche Musterlösung.
+# Modul Projekt Applikation.
+# ----------------------------
+import py5
+
+
+def setup():
+    py5.size(600, 600)
+
+
+def draw():
+    helligkeit = py5.mouse_y / 2.35
+    print(helligkeit)
+    py5.background(helligkeit, helligkeit, helligkeit)
+    py5.ellipse(300, 300, py5.mouse_x, py5.mouse_x)
+
+
+py5.run_sketch()
